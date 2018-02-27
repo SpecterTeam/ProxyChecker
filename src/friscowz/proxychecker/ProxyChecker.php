a27cefee4aa4cfa526afeb952a294ce7b1cf4b61
Fris <ilyesou.cherfaoui.18@gmail.com>
2018-02-27 14:22:50 -0500

    Add files via upload

---
+++ src/friscowz/proxychecker/ProxyChecker.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *     ProxyChecker  Copyright (C) 2018  SpecterTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace friscowz\proxychecker;
+
+
+use friscowz\proxychecker\task\ProxyCheckerTask;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class ProxyChecker extends PluginBase implements Listener
+{
+    /** @var Config */
+    public static $config;
+    /** @var ProxyChecker */
+    public static $instance;
+
+    public function onEnable()
+    {
+        self::setInstance($this);
+        if(!is_dir($this->getDataFolder())) @mkdir($this->getDataFolder());
+        $readme = "This plugin is made by @OGFris (github.com/OGFris), the plugin uses an Online API also made by @OGFris to check the IP." . PHP_EOL . " If you want the process to be 3 times FASTER, contact @OGFris on twitter or email support@legacyhcf.net to buy a license key as cheap as $10 for lifetime!";
+        if (file_exists($this->getDataFolder() . "readme.txt")){
+            if(file_get_contents($this->getDataFolder() . "readme.txt") != $readme){
+                file_put_contents($this->getDataFolder() . "readme.txt", $readme);
+            }
+        } else {
+            $f = fopen($this->getDataFolder() . "readme.txt", "w");
+            fwrite($f, $readme);
+            fclose($f);
+        }
+        if (!file_exists($this->getDataFolder() . "config.yml")){
+            $config = new Config($this->getDataFolder() . "config.ml", Config::YAML, ["key" => "null", "kick" => true, "ban" => false, "kick-message" => "You can't use Proxies/VPN on this server because it's protected with ProxyChecker.ga !"]);
+            self::setConfigData($config);
+        }
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN . "Plugin loaded successfully ! made by @OGFris (github.com/OGFris)");
+    }
+
+    /**
+     * @return Config
+     */
+    public static function getConfigData() : Config
+    {
+        return self::$config;
+    }
+
+    /**
+     * @param Config $config
+     */
+    public static function setConfigData(Config $config)
+    {
+        self::$config = $config;
+    }
+
+    /**
+     * @return ProxyChecker
+     */
+    public static function getInstance() : ProxyChecker
+    {
+        return self::$instance;
+    }
+
+    /**
+     * @param ProxyChecker $instance
+     */
+    public static function setInstance(ProxyChecker $instance)
+    {
+        self::$instance = $instance;
+    }
+
+    /**
+     * @param PlayerPreLoginEvent $event
+     */
+    public function onPreLogin(PlayerPreLoginEvent $event)
+    {
+        $player = $event->getPlayer();
+        $ip = $player->getAddress();
+        $name = $player->getName();
+        $this->getServer()->getScheduler()->scheduleAsyncTask(new ProxyCheckerTask($ip, $name, self::getConfigData()->get("key")));
+    }
+}
---
+++ src/friscowz/proxychecker/task/ProxyCheckerTask.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ *     ProxyChecker  Copyright (C) 2018  SpecterTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace friscowz\proxychecker\task;
+
+
+use friscowz\proxychecker\ProxyChecker;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class ProxyCheckerTask extends AsyncTask
+{
+    /** @var string */
+    private $ip;
+    /** @var string */
+    private $player;
+    /** @var string */
+    private $key;
+
+    /**
+     * Actions to execute when run
+     *
+     * @return void
+     */
+    public function onRun()
+    {
+        $result = json_decode(file_get_contents("http://checkproxy.ga/api/proxychecker/index.php?ip=" . $this->getIp() . "&key=" . $this->getKey()));
+        $this->setResult($result);
+    }
+
+    /**
+     * @param Server $server
+     */
+    public function onCompletion(Server $server)
+    {
+        $results = $this->getResult();
+        switch($results["status"]){
+            case 0:
+                //Proxy detected!
+                $server->getLogger()->debug(TextFormat::RED . $this->getPlayer() . " is using a Proxy/VPN!");
+                $player = $server->getPlayer($this->getPlayer());
+                if ($player){
+                    if (ProxyChecker::getConfigData()->get("kick") == true){
+                        $player->kick(ProxyChecker::getConfigData()->get("kick-message"), false);
+                    }
+                    if (ProxyChecker::getConfigData()->get("ban") == true){
+                        $player->setBanned(true);
+                    }
+                }
+            break;
+
+            case 1:
+                //No Proxy detected!
+                $server->getLogger()->debug(TextFormat::GREEN . $this->getPlayer() . " isn't using a Proxy/VPN!");
+            break;
+
+            case 2:
+                //Error!
+                $server->getLogger()->info(TextFormat::RED . "an error has occurred while trying to check " . $this->getPlayer() . "'s IP! Please make you sure to report this error on our github: github.com/SpecterTeam.");
+            break;
+        }
+    }
+
+    /**
+     * ProxyCheckerTask constructor.
+     * @param string $ip
+     * @param string $player
+     * @param string $key
+     */
+    public function __construct(string $ip, string $player, string $key = "null")
+    {
+        $this->setIp($ip);
+        $this->setPlayer($player);
+        $this->setKey($key);
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlayer(): string
+    {
+        return $this->player;
+    }
+
+    /**
+     * @param string $player
+     */
+    public function setPlayer(string $player)
+    {
+        $this->player = $player;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp() : string
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function setIp(string $ip)
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey() : string
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     */
+    public function setKey(string $key)
+    {
+        $this->key = $key;
+    }
+}
